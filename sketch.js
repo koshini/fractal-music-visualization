053db39b2379a6735da0fa276f2bf8fc008f479b
Shini Ko <ko.shmily@gmail.com>
2018-01-11 00:53:05 -0500

    fractal tree based on amplitude

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
-var song, analyzer;
+var theta, song, analyzer;
 
 function preload() {
   song = loadSound('media/music.mp3');
 }
 
 function setup() {
-  createCanvas(710, 200);
+  createCanvas(640, 640);
   song.loop()
 
-  // create a new Amplitude analyzer
+  // Create a new Amplitude analyzer
   analyzer = new p5.Amplitude();
 
   // Patch the input to an volume analyzer
@@ -18,19 +18,37 @@ function setup() {
 function draw() {
   background(255);
 
-  // Get the average (root mean square) amplitude
-  var rms = analyzer.getLevel();
-  fill(127);
-  stroke(0);
+  level = analyzer.getLevel()*400;
+  // amplitude = (level, 0,1,0,200)
+  theta = radians(level)
+  //theta = map(mouseX,0,width,0,PI/2);
 
-  // Draw an ellipse with size based on volume
-  ellipse(width/2, height/2, 10+rms*200, 10+rms*200);
+  // The first branch starts at the bottom of the window.
+  translate(width/2, height);
+  stroke(0);
+  branch(160);
 }
 
-function mousePressed() {
-  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
-    song.pause();
-  } else {
-    song.play()
+function branch(a) {
+  line(0, 0, 0, -a);
+  translate(0, -a);
+  // each branch will be 2/3 long as the previous one
+  a *= 0.66;
+
+  //exit recursion at length smaller or equal to 2
+  if (a > 2) {
+    push();
+    rotate(theta);
+    // Subsequent calls to branch()
+    // TO-DO
+    // how do I "pause" ?
+    setTimeout(branch(a),0);
+    pop();
+
+    //repeat but rotate to the opposite direction
+    push();
+    rotate(-theta);
+    setTimeout(branch(a),50);
+    pop();
   }
 }
