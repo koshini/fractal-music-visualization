1cb80e1b6d9eb80b14d613e0aad8b74f11f53b52
Shini Ko <ko.shmily@gmail.com>
2018-01-11 23:08:36 -0500

    Add FFT analyzer to get waveform and spectrum.

--- sketch.js
+++ sketch.js
@@ -1,54 +1,61 @@
-var theta, song, analyzer;
+var theta1, song, fft, spectrum, waveform;
 
 function preload() {
-  song = loadSound('media/music.mp3');
+    song = loadSound('media/music.mp3');
 }
 
 function setup() {
-  createCanvas(640, 640);
-  song.loop()
+    createCanvas(640, 640);
+    song.play();
 
-  // Create a new Amplitude analyzer
-  analyzer = new p5.Amplitude();
-
-  // Patch the input to an volume analyzer
-  analyzer.setInput(song);
+    // Create a new FFT to analyze
+    fft = new p5.FFT();
+    fft.setInput(song);
+    spectrum = fft.analyze();
+    waveform = fft.waveform();
 }
 
 function draw() {
-  background(255);
+    background(255);
 
-  level = analyzer.getLevel()*400;
-  // amplitude = (level, 0,1,0,200)
-  theta = radians(level)
-  //theta = map(mouseX,0,width,0,PI/2);
 
-  // The first branch starts at the bottom of the window.
-  translate(width/2, height);
-  stroke(0);
-  branch(160);
+    // The first branch starts at the bottom of the window.
+    translate(width/2, height);
+    stroke(0);
+    for (var i = 0; i < waveform.length; i++) {
+        theta1 = map(spectrum[i], 0, 255, 0, PI / 2);
+        theta2 = map(waveform[i], -1, 1, 0, PI / 2);
+    }
+    branch(160)
+
 }
+//
+// function getAmp(analyzer){
+//   level = analyzer.getLevel()*400;
+//   return level
+// }
 
 function branch(a) {
-  line(0, 0, 0, -a);
-  translate(0, -a);
-  // each branch will be 2/3 long as the previous one
-  a *= 0.66;
+    line(0, 0, 0, -a);
+    translate(0, -a);
 
-  //exit recursion at length smaller or equal to 2
-  if (a > 2) {
-    push();
-    rotate(theta);
-    // Subsequent calls to branch()
-    // TO-DO
-    // how do I "pause" ?
-    setTimeout(branch(a),0);
-    pop();
+    // each branch will be 2/3 long as the previous one
+    a *= 0.66;
 
-    //repeat but rotate to the opposite direction
-    push();
-    rotate(-theta);
-    setTimeout(branch(a),50);
-    pop();
-  }
+    //exit recursion at length smaller or equal to 2
+    if (a > 2) {
+        push();
+        rotate(theta1);
+        // Subsequent calls to branch()
+        // setTimeout(branch(a),500);
+        branch(a);
+        pop();
+
+        //repeat but rotate to the opposite direction
+        push();
+        rotate(-theta2);
+        // setTimeout(branch(a),500);
+        branch(a);
+        pop();
+    }
 }
