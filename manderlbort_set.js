4c5ebb0273af8d6543a13145793e9d4708e84038
Shini Ko <ko.shmily@gmail.com>
2018-01-23 00:06:02 -0500

    Zoom in/out according to amplitude

--- manderlbort_set.js
+++ manderlbort_set.js
@@ -1,6 +1,8 @@
+var scalar = 5;
+var xTranslate = 5;
+var yTranslate;
+var maxIterations = 50;
 var song, analyzer;
-var max_iteration = 100;
-var y_translate, x_translate;
 
 function preload() {
     song = loadSound('media/music.mp3');
@@ -9,55 +11,61 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight);
     pixelDensity(1);
+    yTranslate = height / 2 - width / 2;
 
-    // song.play();
-    //
-    // // create a new Amplitude analyzer
-    // analyzer = new p5.Amplitude();
-    //
-    // // Patch the input to an volume analyzer
-    // analyzer.setInput(song);
+    song.loop();
+
+    // create a new Amplitude analyzer
+    analyzer = new p5.Amplitude();
+
+    // Patch the input to an volume analyzer
+    analyzer.setInput(song);
+
 }
 
 function draw() {
     loadPixels();
-    for (var x = 0; x < width; x++){
+    var amp = analyzer.getLevel();
+    var coefficient = map(amp, 0, 1, 1, 6);
+
+    for (var x = 0; x < width; x++) {
         for (var y = 0; y < height; y++) {
-            var a = map(x, 0, width, -1.5, 1.5);
-            var b = map(y, 0, height, -2.5, 2.5);
+            var val = coefficient;
+
+            var a = map(x, 0 , width, -val, val);
+            var b = map(y, 0 + yTranslate, width + yTranslate, -val, val);
+
+
+            // var a = map(x, 0 + xTranslate, width + xTranslate, -scalar + coefficient, scalar + coefficient);
+            // var b = map(y, 0 + yTranslate, width + yTranslate, -scalar + coefficient, scalar + coefficient);
 
             var ca = a;
             var cb = b;
-
             var n = 0;
-
-            while (n < max_iteration) {
+            while (n < maxIterations) {
                 var aa = a * a - b * b;
                 var bb = 2 * a * b;
-
                 a = aa + ca;
                 b = bb + cb;
-
-                if (abs(a + b) > 16) {
+                if (a + b > 16) {
                     break;
                 }
                 n++;
             }
-
-            var brightness = map(n, 0, max_iteration, 0, 1);
+            var brightness = map(n, 0, maxIterations, 0, 1);
             brightness = map(sqrt(brightness), 0, 1, 0, 255);
-            if (n === max_iteration) {
+            if (n === maxIterations) {
                 brightness = 0;
             }
 
-             var pix = (x + y * width) * 4;
-             pixels[pix] = brightness;
-             pixels[pix + 1] = brightness;
-             pixels[pix + 2] = brightness;
-             pixels[pix + 3] = 255;
-            }
+            var pix = (x + y * width) * 4;
+            pixels[pix] = brightness;
+            pixels[pix + 1] = brightness;
+            pixels[pix + 2] = brightness;
+            pixels[pix + 3] = 255;
         }
-    updatePixels();
     }
-
-
+    updatePixels();
+    scalar *= 0.9;
+    xTranslate = (1 / scalar) * (width * 0.705) ;
+}
